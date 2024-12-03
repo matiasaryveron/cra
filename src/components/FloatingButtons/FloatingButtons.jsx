e47dcc3bd21625ea73a822f687eb86f97b941da8
Matias Ary Veron <veron-matias@hotmail.com>
2024-12-03 15:50:16 -0300

    mensajewp

--- src/components/FloatingButtons/FloatingButtons.jsx
+++ src/components/FloatingButtons/FloatingButtons.jsx
@@ -6,7 +6,7 @@ function FloatingButtons() {
 return (
     <div className="fixed bottom-4 right-4 flex flex-col items-center gap-2">
     <a
-        href="https://wa.me/5491133320582"
+        href="https://wa.me/5491133320582?text=Hola%20%F0%9F%91%8B%2C%20te%20contacto%20desde%20la%20p%C3%A1gina%20web%2C%20quiero%20recibir%20m%C3%A1s%20informaci%C3%B3n."
         target="_blank"
         rel="noopener noreferrer"
         className="bg-green-500 p-3 rounded-full shadow-lg text-white hover:bg-green-600 transition-colors duration-300"
